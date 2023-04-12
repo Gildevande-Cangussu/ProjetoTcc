5a741137320685093755d66c5152841fb81c01d4
Gildevande-Cangussu <gildevande.silva@etec.sp.gov.br>
2023-04-12 20:32:39 -0300

    no

--- src/html/map.js
+++ src/html/map.js
@@ -1,13 +1,21 @@
 // Token de acesso Mapbox
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lsZGV2YW5kZSIsImEiOiJjbDgyYmp2b2kwb2RpM3dvM25uM2QxZ3BjIn0.7YJltuO14MOrUeqVXCwsnA';
-
 // Criação do mapa
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
     container: 'map', // ID do contêiner HTML do mapa
-    style: 'mapbox://styles/gildevande/clge8s6le004201o1tjxbc4j8', // Estilo do mapa (pode ser alterado)
+    style: 'mapbox://styles/gildevande/clge8s6le004201o1tjxbc4j8',  // Estilo do mapa (pode ser alterado)
     center: [-46.4222, -23.5000], // Coordenadas do centro do mapa (São Miguel Paulista)
-    zoom: 13 // Nível de zoom inicial do mapa
+    zoom: 13, // Nível de zoom inicial do mapa
 });
+//adicionando caixa de busca ao mapa//
+const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+    placeholder: "Pesquisar endereço",
+    marker: false,
+  });
+  document.getElementById("procurar-endereco").appendChild(geocoder.onAdd(map));
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lsZGV2YW5kZSIsImEiOiJjbDgyYmp2b2kwb2RpM3dvM25uM2QxZ3BjIn0.7YJltuO14MOrUeqVXCwsnA';
 
 // Adição dos pontos de coleta
 
